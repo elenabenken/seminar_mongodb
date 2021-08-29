1dfe4a30a3131b27542def8832bb66b79d73d2ab
elenabenken <elenabenken@users.noreply.github.com>
2021-08-29 09:48:20 +0300

    Update 03_http.js
    изменения для хуков

--- examples/03_http.js
+++ examples/03_http.js
@@ -3,7 +3,7 @@ var server = http.createServer(
 function (request, response) {
   response.writeHead(200,
    {"Content-Type": "text/plain"});
-  response.end( "Наш сайт");
+  response.end( "Наш общий сайт");
 });
 server.listen(8000);
 console.log("Сервер работает на http://127.0.0.1:8000/");
