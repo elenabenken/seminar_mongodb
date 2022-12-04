82375f7cb69b6d14c2e79bc7e15eaf843eb0b1af
elenabenken <elenabenken@users.noreply.github.com>
2022-12-04 12:45:45 +0300

    Update 03_http.js

--- examples/03_http.js
+++ examples/03_http.js
@@ -3,7 +3,7 @@ var server = http.createServer(
 function (request, response) {
   response.writeHead(200,
    {"Content-Type": "text/plain"});
-  response.end( "Наш сайт");
+  response.end( "Сайт");
 });
 server.listen(8000);
 console.log("Сервер на http://127.0.0.1:8000/");
