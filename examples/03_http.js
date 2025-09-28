c29beec5fa711b152f77c5fa4a28a8866492d006
elenabenken <elenabenken@users.noreply.github.com>
2025-09-28 10:50:17 +0300

    Update 03_http.js

--- examples/03_http.js
+++ examples/03_http.js
@@ -3,7 +3,7 @@ var server = http.createServer(
 function (request, response) {
   response.writeHead(200,
    {"Content-Type": "text/plain"});
-  response.end( "Это наш сайт");
+  response.end( "Это наш красивый сайт");
 });
 server.listen(8000);
 console.log("Сервер новый текст на http://localhost/");
